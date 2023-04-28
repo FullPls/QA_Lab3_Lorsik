9775a90e2b9ed89ee8d6db25b0921ea95a4f8863
FullPls <131536200+FullPls@users.noreply.github.com>
2023-04-28 23:36:07 +0700

    Update Jewerely_store.java

--- QA_Lab3_Kosik/Lab6.1/src/Lab6_2/Jewerely_store.java
+++ QA_Lab3_Kosik/Lab6.1/src/Lab6_2/Jewerely_store.java
@@ -4,14 +4,10 @@ public class Jewerely_store extends Jawel {
    private String name;
     private Jawel a = new Jawel();
     private Jawel b = new Jawel();
-    private int x;
-    private int y;
-    public void Init(String n, int v1, int c1, int x1, int y1)
+    public void Init(String n, int v1, int c1)
     {
         name = n;
         super.Init(v1, c1);
-        x = x1;
-        y = y1;
     }
     public void Display()
     {
