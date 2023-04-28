b5333c6f5bd87c83ef6e85cd3702ca8ddaaeb82b
FullPls <131536200+FullPls@users.noreply.github.com>
2023-04-28 23:35:42 +0700

    Update Main.java

--- QA_Lab3_Kosik/Lab6.1/src/Lab6_2/Main.java
+++ QA_Lab3_Kosik/Lab6.1/src/Lab6_2/Main.java
@@ -5,7 +5,7 @@ import Lab6_2.Jawel;
 public class Main {
     public static void main(String[] args) {
         New_b_class  bb = new New_b_class ();
-        bb.Init("Shiny", 100, 100, 300, 300, 20, 10);
+        bb.Init("Shiny", 100, 100, 300, 300);
         bb.Display();
         System.out.println("Удвоенная сумма: " + bb.summary_cost( ));
     }
