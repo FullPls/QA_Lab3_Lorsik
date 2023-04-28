1b130e3b29139b819f8d3a65a98d2e4db8b978f1
FullPls <131536200+FullPls@users.noreply.github.com>
2023-04-28 23:36:42 +0700

    Update New_b_class.java

--- QA_Lab3_Kosik/Lab6.1/src/Lab6_2/New_b_class.java
+++ QA_Lab3_Kosik/Lab6.1/src/Lab6_2/New_b_class.java
@@ -4,12 +4,11 @@ public class New_b_class {
     private String name;
     private Jawel S = new Jawel();
     private Jewerely_store  R = new Jewerely_store ();
-    public void Init(String n, int v1, int c1, int v2, int c2, int x1, int y1/*, int weight1, int cost1*/)
+    public void Init(String n, int v1, int c1, int v2, int c2)
     {
         name = n;
         S.Init(v2, c2);
         R.Init(n, v1, c1, /*v2, c2,*/ x1, y1);
-
     }
     public void Display()
     {
